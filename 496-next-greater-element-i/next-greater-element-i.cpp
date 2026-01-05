a8ce3d55145ef0e558f488bc53c9b7dc7628fc2b
Rupesh Kumar <samratrupesh08@gmail.com>
2026-01-05 12:17:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (35.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,4 +1,5 @@
-class Solution{
+//BRUTE FORCE APPROACH--------------------------------------------
+/*class Solution{
 public:
     vector<int> nextGreaterElement(vector<int>&nums1,vector<int>&nums2){
         vector<int>result;
@@ -18,6 +19,32 @@ public:
             }
             result.push_back(ans);
         }
+        return result;
+    }
+};
+*/
+
+//OPTIMAL APPROACH------------------------------------
+class Solution{
+public:
+    vector<int> nextGreaterElement(vector<int>&nums1,vector<int>&nums2){
+        vector<int>result;
+        unordered_map<int,int>m;
+        stack<int>s;
+        for(int i=0;i<nums2.size();i++){
+            while(!s.empty() && s.top()<nums2[i]){
+                m[s.top()]=nums2[i];
+                s.pop();
+            }
+            s.push(nums2[i]);
+        }
+        while(!s.empty()){
+        m[s.top()]=-1;
+        s.pop();
+         }
+         for(int i=0;i<nums1.size();i++)
+           result.push_back(m[nums1[i]]);
+
         return result;
     }
 };
