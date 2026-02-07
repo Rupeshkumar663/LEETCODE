a1b986ab277269533641d43bba74a8cc9a301f65
Rupesh Kumar <samratrupesh08@gmail.com>
2026-02-07 12:32:43 +0530

    Time: 28 ms (87.16%) | Memory: 115.5 MB (99.87%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,3 +1,6 @@
+
+//APPROACH-1 USING MAP AND DFS----------------------------------------
+/*
 class Solution {
 public:
 void DFS(unordered_map<int,vector<int>>&adj,int u, vector<bool>&visited,int &nodes,int &edge){
@@ -31,4 +34,40 @@ void DFS(unordered_map<int,vector<int>>&adj,int u, vector<bool>&visited,int &nod
         }
         return result;
     }
+};
+*/
+//APPROACH-2  DFS AND WITHOUT MAP----------------------------------------
+class Solution{
+public:
+void DFS(int u,vector<vector<int>>&edges,vector<bool>&visited,int &nodes,int &edge){
+    visited[u]=true;
+    nodes++;
+    for(auto &e:edges){
+        int a=e[0];
+        int b=e[1];
+        if(a==u||b==u){
+            edge++;
+            int next=(a==u)?b:a;
+            if(!visited[next]){
+                DFS(next,edges,visited,nodes,edge);
+            }
+        }
+    }
+}
+int countCompleteComponents(int n,vector<vector<int>>&edges){
+    vector<bool>visited(n,false);
+    int result=0;
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+            int nodes=0;
+            int edge=0;
+            DFS(i,edges,visited,nodes,edge);
+            edge=edge/2;
+            if(edge==nodes*(nodes-1)/2){
+                result++;
+            }
+        }
+    }
+    return result;
+}
 };
