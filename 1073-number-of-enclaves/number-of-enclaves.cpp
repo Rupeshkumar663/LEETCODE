5edecfdb67a324ee3a50e08c73bec4aa9356f3c2
Rupesh Kumar <samratrupesh08@gmail.com>
2026-02-07 14:10:22 +0530

    Time: 46 ms (9.12%) | Memory: 39.2 MB (13.98%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,3 +1,4 @@
+/*//APPROACH-1 BY USING DFS-----------------------------------------
 class Solution {
 public:
   void DFS(vector<vector<int>>& grid,int i,int j,int &m,int &n){
@@ -24,6 +25,73 @@ public:
         int result=0;
          for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
+                if(grid[i][j]==1)
+                    result++;
+            }
+        }
+        return result;
+    }
+};
+*/
+//APPROACH-2 BY USING DFS-----------------------------------------
+class Solution {
+public:
+  void BFS(vector<vector<int>>& grid, vector<vector<int>>& temp){
+    queue<vector<int>>q;
+
+    for(int i=0;i<temp.size();i++){
+        int x=temp[i][0];
+        int y=temp[i][1];
+        grid[x][y]=0;
+        q.push({x,y});
+    }
+
+    while(!q.empty()){
+        vector<int>num=q.front();
+        q.pop();
+        int x=num[0];
+        int y=num[1];
+
+        if(x-1>=0 && grid[x-1][y]==1){
+            q.push({x-1,y});
+            grid[x-1][y]=0;
+        }
+        if(x+1<grid.size() && grid[x+1][y]==1){
+            q.push({x+1,y});
+            grid[x+1][y]=0;
+        }
+        if(y-1>=0 && grid[x][y-1]==1){
+            q.push({x,y-1});
+            grid[x][y-1]=0;
+        }
+        if(y+1<grid[0].size() && grid[x][y+1]==1){
+            q.push({x,y+1});
+            grid[x][y+1]=0;
+        }
+    }
+  }
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        if(grid.size()==0)
+           return 0;
+
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>>temp;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && (i==0 || j==0 || i==m-1 || j==n-1)){
+                    temp.push_back({i,j});
+                }
+            }
+        }
+
+        BFS(grid,temp);
+
+        int result=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1)
                     result++;
             }
