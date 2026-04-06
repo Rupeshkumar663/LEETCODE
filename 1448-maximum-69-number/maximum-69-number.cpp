cf788fe514c0fb375202bf35856f41466511832a
Rupesh Kumar <samratrupesh08@gmail.com>
2026-04-07 02:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (9.44%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int maximum69Number (int num) {
@@ -10,4 +11,22 @@ public:
        }
        return stoi(s);
     }
+};
+*/
+class Solution {
+public:
+    int maximum69Number (int num) {
+       int temp1=-1;
+       int temp2=0;
+       int temp=num;
+       while(temp>0){
+          if(temp%10==6){
+             temp1=temp2;
+          }
+          temp/=10;
+          temp2++;
+       }
+       num=num+3*pow(10,temp1);
+      return num;
+    }  
 };
