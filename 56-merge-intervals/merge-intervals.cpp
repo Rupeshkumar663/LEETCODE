b8cb0ee8c3f3da5c8ae8f2235e8131a73da9bcdf
Rupesh Kumar <samratrupesh08@gmail.com>
2026-04-09 23:01:44 +0530

    Time: 4 ms (66.93%) | Memory: 23.9 MB (32.02%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-        if(intervals.size()==1)
-           return intervals;
+
         vector<vector<int>>result;
         int start=intervals[0][0];
         int end=intervals[0][1];
