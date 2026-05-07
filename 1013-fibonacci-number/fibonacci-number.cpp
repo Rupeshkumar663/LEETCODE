e3680086d4b920158816c34094409890dfb03a1a
Rupesh Kumar <samratrupesh08@gmail.com>
2026-05-07 23:34:05 +0530

    Time: 3 ms (62.72%) | Memory: 8.3 MB (11.42%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,30 @@
+//O(2^n)
+//Recursion
+/*
 class Solution {
 public:
     int fib(int n) {
         if(n==0 || n==1)
          return n;
         return fib(n-1)+fib(n-2);
+    }
+};
+*/
+
+class Solution {
+public:
+    int solve(int n,vector<int>&dp){
+      if(n==0 || n==1)
+         return n;
+         if(dp[n]!=-1){
+            return dp[n];
+         }
+         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+   }
+    int fib(int n) {
+
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+        
     }
 };
