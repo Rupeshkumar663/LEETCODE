f626340b38dd0639cffd61582525a59177091719
Rupesh Kumar <samratrupesh08@gmail.com>
2026-05-08 00:13:19 +0530

    Time: 2 ms (76.87%) | Memory: 7.7 MB (88.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -30,7 +30,10 @@ public:
     }
 };
 */
-
+//T.C-O(n)
+//S.C-O(n)
+//Bottom-up Approach---
+/*
 class Solution {
 public:
     int fib(int n) {
@@ -44,5 +47,23 @@ public:
             dp[i]=dp[i-1]+dp[i-2];
          }
     return dp[n];
+    }
+};
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)
+          return n;
+       int a=0;
+       int b=1;
+       int c=1;
+       for(int i=1;i<n;i++){
+         c=a+b;
+         a=b;
+         b=c;
+       }
+       return c;
     }
 };
