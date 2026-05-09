93d1e2c7bad18a08848b19c26abe5940335cbad7
Rupesh Kumar <samratrupesh08@gmail.com>
2026-05-10 00:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (59.28%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -20,6 +20,9 @@ public:
     }
 };
 */
+//T.c-O(n)
+//s.c-O(n)
+/*
 class Solution {
 public:
 int dp[101];
@@ -38,5 +41,23 @@ int dp[101];
        memset(dp,-1,sizeof(dp));
        return solve(n-1,nums);
      
+    }
+};
+*/
+class Solution {
+public:
+
+ 
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+       vector<int>dp(n+1,0);
+       dp[0]=0;
+       dp[1]=nums[0];
+       for(int i=2;i<=n;i++){
+        int steal=nums[i-1]+dp[i-2];
+        int skip=dp[i-1];
+         dp[i]=max(steal,skip);
+       }
+       return dp[n];
     }
 };
