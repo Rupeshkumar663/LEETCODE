972b4e7937b2b78f39213eea977b4d9dab3c4948
Rupesh Kumar <samratrupesh08@gmail.com>
2026-05-09 22:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.48%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -59,7 +59,7 @@ public:
       return dp[n];
     }
 };
-*/
+
 class Solution {
 public: 
     int climbStairs(int n) {
@@ -75,5 +75,25 @@ public:
         b=c;
      }
      return c;
+    }
+};
+*/
+class Solution {
+public: 
+int solve(int n,vector<int>&dp){
+    if(n<0){
+        return 0;
+    }
+    if(n==0){
+        return 1;
+    }
+    if(dp[n]!=-1)
+      return dp[n];
+    return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+  }
+    int climbStairs(int n) {
+      vector<int>dp(n+1,-1);
+     return solve(n,dp);
+      
     }
 };
