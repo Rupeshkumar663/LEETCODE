423ab84ba3f9a011695a1ee93738bb7e540e36c6
Rupesh Kumar <samratrupesh08@gmail.com>
2026-05-16 12:04:01 +0530

    Time: 43 ms (46.58%) | Memory: 27.3 MB (50.53%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -66,9 +66,9 @@ public:
             for(int j=0;j<i;j++){
                 if(pairs[i][0]>pairs[j][1]){
                     dp[i]=max(dp[i],dp[j]+1);
-                }
-                result=max(result,dp[i]);
+                } 
             }
+            result=max(result,dp[i]);
         }
         return result;
     }
