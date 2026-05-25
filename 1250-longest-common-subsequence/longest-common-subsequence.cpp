42ec164caa07c1277002b5ea137ee7b46eedb085
Rupesh Kumar <samratrupesh08@gmail.com>
2026-05-26 00:06:18 +0530

    Time: 27 ms (55.25%) | Memory: 27.4 MB (50.16%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -20,6 +20,10 @@ int solve(string &text1, string &text2,int i,int j){
        return solve(text1,text2,0,0);
     }
 };*/
+//Recursion+memoization
+//T.C-O(m*n)
+//S.C-O(m*n)
+/*
 
 class Solution {
 public:
@@ -43,5 +47,32 @@ int solve(string &text1, string &text2,int i,int j){
         n=text2.size();
         memset(dp,-1,sizeof(dp));
        return solve(text1,text2,0,0);
+    }
+};*/
+
+class Solution {
+public:
+
+    int longestCommonSubsequence(string text1, string text2) {
+       int  m=text1.size();
+        int n=text2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1));
+       for(int row=0;row<m+1;row++){
+         dp[row][0]=0;
+       }
+       for(int col=0;col<n+1;col++){
+         dp[0][col]=0;
+       }
+
+       for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(text1[i-1]==text2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            } else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+         }
+       }
+       return dp[m][n];
     }
 };
