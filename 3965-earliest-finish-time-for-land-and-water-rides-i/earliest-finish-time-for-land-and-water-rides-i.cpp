e31e337c1254629b0514cb48599f1e431497916f
Rupesh Kumar <samratrupesh08@gmail.com>
2026-06-02 17:25:35 +0530

    Time: 12 ms (29.96%) | Memory: 98.7 MB (6.33%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -15,9 +15,8 @@ public:
        for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
            int temp1 = max(land[i][0] + land[i][1], water[j][0]) + water[j][1];
-
-int temp2 = max(water[j][0] + water[j][1], land[i][0]) + land[i][1];
-            result=min(result,min(temp1,temp2));
+           int temp2 = max(water[j][0] + water[j][1], land[i][0]) + land[i][1];
+           result=min(result,min(temp1,temp2));
         }
        }
       return result;
