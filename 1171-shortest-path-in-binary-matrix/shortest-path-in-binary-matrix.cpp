7ff2788bb575ac7fee96ebf021304e6ca91a935c
Rupesh Kumar <samratrupesh08@gmail.com>
2026-06-05 01:44:36 +0530

    Time: 28 ms (29.46%) | Memory: 22.9 MB (99.07%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -59,67 +59,58 @@ public:
         return result;
     }
 };*/
-
+//T.C-OO(n^2*logn)
+//T.C-O(n*n)
+//Dijkstra Algorithm --------------
 class Solution {
 public:
-int n;
-void BFS(vector<vector<int>>& grid,int &result){
-    queue<pair<int,int>>q;
-    q.push({0,0});
-    grid[0][0]=1;
-    while(!q.empty()){
-        int x=q.size();
-        while(x--){
-        pair<int,int>temp=q.front();
-        q.pop();
-        int i=temp.first;
-        int j=temp.second;
-        if(i==n-1 && j==n-1)
-             return;
-          if((i-1)>=0 && i<n && grid[i-1][j]==0){//upward
-              q.push({i-1,j});
-              grid[i-1][j]=1;
-           }
-          if((i-1)>=0 && i<n && (j-1)>=0 && j<n && grid[i-1][j-1]==0){//left-up corner
-              q.push({i-1,j-1});
-              grid[i-1][j-1]=1;
-           }
-          if((j-1)>=0 && j<n && grid[i][j-1]==0){//left
-              q.push({i,j-1});
-              grid[i][j-1]=1;
-          }
-          if( (i+1)<n && (j-1)>=0 && grid[i+1][j-1]==0){//left-down corner
-              q.push({i+1,j-1});
-              grid[i+1][j-1]=1;
-           }
-          if( (i+1)<n && grid[i+1][j]==0){//down
-              q.push({i+1,j});
-              grid[i+1][j]=1;
-           }
-         if( (i+1)<n && (j+1)<n && grid[i+1][j+1]==0){//right-down corner
-              q.push({i+1,j+1});
-              grid[i+1][j+1]=1;
-         }
-         if(  (j+1)<n && grid[i][j+1]==0){//right
-              q.push({i,j+1});
-              grid[i][j+1]=1;
-          }
-         if( (i-1)>=0 && (j+1)<n && grid[i-1][j+1]==0){//right-up corner
-              q.push({i-1,j+1});
-              grid[i-1][j+1]=1;
-         }
-        }
-        result++;
-    }
-    result=-1;
-}
+
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        n=grid.size();
+       int  n=grid.size();
         if(grid[0][0]==1 || grid[n-1][n-1]==1){
             return -1;
         }
-        int result=1;
-         BFS(grid,result);
-         return result;
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        pq.push({1,0,0});
+        while(!pq.empty()){
+            auto [result,i,j]=pq.top();
+            pq.pop();
+            if(i==n-1 && j==n-1){
+                return result;
+            }
+             if((i-1)>=0 && i<n && grid[i-1][j]==0){//upward
+             pq.push({result+1,i-1,j});
+              grid[i-1][j]=1;
+           }
+          if((i-1)>=0 && i<n && (j-1)>=0 && j<n && grid[i-1][j-1]==0){//left-up corner
+             pq.push({result+1,i-1,j-1});
+              grid[i-1][j-1]=1;
+           }
+          if((j-1)>=0 && j<n && grid[i][j-1]==0){//left
+             pq.push({result+1,i,j-1});
+              grid[i][j-1]=1;
+          }
+          if( (i+1)<n && (j-1)>=0 && grid[i+1][j-1]==0){//left-down corner
+             pq.push({result+1,i+1,j-1});
+              grid[i+1][j-1]=1;
+           }
+          if( (i+1)<n && grid[i+1][j]==0){//down
+             pq.push({result+1,i+1,j});
+              grid[i+1][j]=1;
+           }
+         if( (i+1)<n && (j+1)<n && grid[i+1][j+1]==0){//right-down corner
+             pq.push({result+1,i+1,j+1});
+              grid[i+1][j+1]=1;
+         }
+         if(  (j+1)<n && grid[i][j+1]==0){//right
+             pq.push({result+1,i,j+1});
+              grid[i][j+1]=1;
+          }
+         if( (i-1)>=0 && (j+1)<n && grid[i-1][j+1]==0){//right-up corner
+             pq.push({result+1,i-1,j+1});
+              grid[i-1][j+1]=1;
+          }    
+        }
+        return -1;
     }
 };
