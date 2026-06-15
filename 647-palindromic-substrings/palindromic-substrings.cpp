fcbd73c0771e713468ac6c9d63d9a8d4a697d044
Rupesh Kumar <samratrupesh08@gmail.com>
2026-06-15 21:12:23 +0530

    Time: 35 ms (25.37%) | Memory: 13 MB (37.86%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,3 +1,4 @@
+/*
 //T.C-O(n^3)---------
 //Brute force Approach---------
 class Solution {
@@ -13,6 +14,39 @@ public:
    }
     int countSubstrings(string s) {
         int n=s.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(check(s,i,j)==true){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+*/
+
+//T.C-O(n^3)---------
+//Brute force Approach---------
+class Solution {
+public:
+  int dp[1001][1001];
+   bool check(string &s,int i,int j){
+    if(i>j){
+        return true;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    if(s[i]==s[j]){
+        return dp[i][j]=check(s,i+1,j-1);
+    }
+   return dp[i][j]=false; 
+   }
+    int countSubstrings(string s) {
+        int n=s.length();
+        memset(dp,-1,sizeof(dp));
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
