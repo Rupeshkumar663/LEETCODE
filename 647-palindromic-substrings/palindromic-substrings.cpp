3341aa6d152676284716a84cca4fc37eb7ae798f
Rupesh Kumar <samratrupesh08@gmail.com>
2026-06-19 21:50:58 +0530

    Time: 27 ms (33.38%) | Memory: 10.9 MB (50.30%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -29,6 +29,7 @@ public:
 
 //T.C-O(n^2)---------
 //Memoization Approach---------
+/*
 class Solution {
 public:
   int dp[1001][1001];
@@ -51,6 +52,35 @@ public:
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(check(s,i,j)==true){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};*/
+
+class Solution {
+public:
+  
+    int countSubstrings(string s) {
+        int n=s.length();
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        int count=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(i==j){
+                    dp[i][j]=true;
+                } else if(i+1==j){
+                    if(s[i]==s[j]){
+                        dp[i][j]=true;
+                    }
+                }else{
+                    if(s[i]==s[j] && dp[i+1][j-1]){
+                        dp[i][j]=true;
+                    }
+                }
+                if(dp[i][j]==true){
                     count++;
                 }
             }
