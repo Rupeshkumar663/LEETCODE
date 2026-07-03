6c6df6be5c3b0a8f5d149b17b67d9298860798dd
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-03 09:44:12 +0530

    Time: 4 ms (85.28%) | Memory: 44.8 MB (78.01%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,16 +1,19 @@
-
+//Time Complexity-O(nlogn)
+//Space Complexity-O(1)
+//Greedy Approach-------------
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
        int m=g.size();
        int n=s.size();
+       //sorting greed factors and cookies size in ascending order
        sort(g.begin(),g.end());
        sort(s.begin(),s.end());
-       int i=0;
-       int count=0;
-       for(int j=0;j<n;j++){
+       int i=0;//it reperesent current child
+       int count=0;//store maximum number of content 
+       for(int j=0;j<n;j++){//traverse all cookies
          if(i<m){
-            if(s[j]>=g[i]){
+            if(s[j]>=g[i]){// if satisfied this condition then count
             i++;
             count++;
             }
