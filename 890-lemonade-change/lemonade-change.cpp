262e536e9a119dfde22323e84fb0b1ab7966330f
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-04 10:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (45.26%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,28 +1,32 @@
+//Time Complexity-O(n)
+//Space Complexity-O(1)
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five=0;
-        int ten=0;
-        for(int i=0;i<bills.size();i++){
-            if(bills[i]==5){
-                five++;
-            } else if(bills[i]==10){
-                    if(five==0)
-                      return false;
-                    five--;
-                    ten++;
-                }
-              else{
-                    if(five>0 && ten >0){
-                        five--;
-                        ten--;
-                    } else if(five>=3){
-                        five-=3;
-                    } else{
-                     return false;
-                    }
-                 }
+      int n=bills.size();//creating a variable to store size of bill
+      int five=0;//sotre five 
+      int ten=0;//store ten
+      for(int i=0;i<n;i++){//Traverse all bills
+        if(bills[i]==5){
+            five++;
+        }else if(bills[i]==10){
+            if(five>=1){
+                five--;
+                ten++;
+            }else{
+                return false;
+            }
+        }else{
+            if(five>=1 && ten>=1){
+                five--;
+                ten--;
+            }else if(five>=3){
+                five-=3;
+            }else{
+                return false;
+            }
         }
-       return true;
+      } 
+      return true; 
     }
 };
