445a7ee7e8829bb169432aa06ad60dc3147afec5
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-06 19:31:51 +0530

    Time: 1 ms (39.17%) | Memory: 17.1 MB (44.72%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -9,30 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //Time Complexity: O(n)
+ //Space COmplexity: O(n)
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root){
        vector<vector<int>>result;
-       queue<TreeNode*>q;
-       if(root==NULL){
+       queue<TreeNode*>q;// Queue used for BFS traversal.level
+       if(root==NULL){//return empty array when tree is empty
         return {};
        }
        q.push(root);
-       while(!q.empty()){
-          int t=q.size();
-          vector<int>num;
-          while(t--){
+       while(!q.empty()){//Time Complexity: O(n)
+          int t=q.size(); // Number of nodes at the current level.
+          vector<int>num;// Stores values of the current level.
+          while(t--){//Time Complexity: O(1)
             TreeNode* temp=q.front();
             q.pop();
             num.push_back(temp->val);
-            if(temp->left){
+            if(temp->left){   // Push left child if it exists.
               q.push(temp->left);
             }
-            if(temp->right){
+            if(temp->right){   // Push right child if it exists.
               q.push(temp->right);
             }
           }
-         result.push_back(num);
+         result.push_back(num);// Store the current level in the final result.
        }
        return result;
     }
