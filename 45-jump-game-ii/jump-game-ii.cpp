8579d8e373728dc29aaf7c5902694378bfe6929d
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-07 11:06:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (43.11%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,16 +1,19 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int maxi=0;
-        int count=0;
-        int current=0;
-        for(int i=0;i<nums.size()-1;i++){
-            maxi=max(maxi,i+nums[i]);
-            if(i==current){
-                count++;
-                current=maxi;
-            }
+     int n=nums.size();
+     int maxi=0;//Store maximum jump can I do.
+     int count=0;//count minimum number of jump 
+     int current=0;//store current index after jump
+     for(int i=0;i<n-1;i++){//Time Complexity: O(n)
+        maxi=max(maxi,i+nums[i]);
+        if(i==current){//if condition satisfied then will jump and count increase by 1.
+            count++;
+            current=maxi;
         }
-     return count;
-    }
+      }
+    return count;//return minimum number of jump to reach last index.
+   }
 };
