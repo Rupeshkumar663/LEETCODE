6a53f7d883ce318d8234fa24bd7f9c6b89931a8f
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-07 21:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (30.99%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -26,6 +26,7 @@ public:
     }
 };
 */
+/*
 //Time COmplexity: O(m*n)
 //Space Complexity: O(m*n)
 //Recursion+Memoization-----------------
@@ -55,5 +56,58 @@ public:
        }
        memset(dp,-1,sizeof(dp));
        return  solve(obstacleGrid,0,0);
+    }
+};
+*/
+
+//Time COmplexity: O(m*n)
+//Space Complexity: O(m*n)
+//Recursion+Memoization-----------------
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+      int  m=obstacleGrid.size();
+      int n=obstacleGrid[0].size();
+      if(obstacleGrid[m-1][n-1]==1){
+        return 0;
+       }
+      vector<vector<int>>dp(m,vector<int>(n));
+      dp[0][0]=1;
+      // 1st row filled-----------
+      for(int j=1;j<n;j++){
+        if(obstacleGrid[0][j]==1){
+            dp[0][j]=0;
+        }
+        else if(j>=1 && obstacleGrid[0][j-1]==1){
+            dp[0][j]=0;
+            obstacleGrid[0][j]=1;
+        }else{
+            dp[0][j]=1;
+        }
+      }
+       //1st column filled-----------
+       for(int i=1;i<m;i++){
+        if(obstacleGrid[i][0]==1){
+            dp[i][0]=0;
+        }
+        else if(i>=1 && obstacleGrid[i-1][0]==1){
+            dp[i][0]=0;
+            obstacleGrid[i][0]=1;
+        }else{
+            dp[i][0]=1;
+        }
+      }
+
+      //rest part will be filled----------
+       for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(obstacleGrid[i][j]==1){
+                dp[i][j]=0;
+            }else{
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+       }
+       return dp[m-1][n-1];
     }
 };
