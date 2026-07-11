a584b9696f9522164b73ffdc8c2cf9cbaa85b486
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-11 12:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (51.89%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
 class Solution {
 public:
    TreeNode* minimum(TreeNode* root){
@@ -46,5 +47,51 @@ public:
             }
         }
          return root;
+    }
+};
+*/
+
+class Solution {
+public:
+   TreeNode* minimum(TreeNode* root){
+    if(root==NULL){
+        return root;
+    }
+     while(root->left!=NULL){
+        root=root->left;
+     }
+     return root;
+   }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val>key){
+            root->left=deleteNode(root->left,key);
+        }
+        else if(root->val<key){
+            root->right=deleteNode(root->right,key);
+        }else{
+            if(root->val==key && (root->left==NULL && root->right==NULL)){
+                delete root;
+                return NULL;
+            }else if(root->right==NULL){
+                TreeNode* temp=root->left;
+                 delete root;
+                 return temp;
+            }
+            else if(root->left==NULL){
+                TreeNode* temp=root->right;
+                 delete root;
+                 return temp;
+            }else{
+                TreeNode *temp=minimum(root->right);
+                root->val=temp->val;
+               root->right=deleteNode(root->right,temp->val);
+               
+            }
+        }
+
+        return root;
     }
 };
