49f9cb839604414e94069ba8a4bb139e22c4f4b7
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-11 10:55:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (47.90%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -22,5 +23,28 @@ public:
             return false;
         }
       return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
+*/
+
+class Solution {
+public:
+ bool solve(TreeNode* p, TreeNode* q){
+    if(p==NULL && q==NULL){
+        return true;
+    }
+    if(p==NULL || q==NULL){
+        return false;
+    }
+    if((p->val==q->val)&& p->left==NULL && p->right==NULL && q->left==NULL && q->right==NULL){
+        return true;
+    }
+    if(p->val!=q->val){
+        return false;
+    }
+    return solve(p->left,q->left) && solve(p->right,q->right);
+ }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+      return  solve(p,q);
     }
 };
