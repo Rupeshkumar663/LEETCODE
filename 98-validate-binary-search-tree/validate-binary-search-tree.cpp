31c9146228edfb362275059c52152be62163c3ea
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-14 11:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (95.27%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool check(TreeNode* root, long long low, long long high) {
         if(!root) 
@@ -10,5 +10,39 @@ public:
 
     bool isValidBST(TreeNode* root) {
         return check(root,LLONG_MIN,LLONG_MAX);
+    }
+};*/
+
+class Solution {
+public:
+   void Inorder(TreeNode*root,long long &prev,bool &result){
+    if(root==NULL){
+        return ;
+    }
+    if(root->left){
+        Inorder(root->left,prev,result);
+    }
+    
+    if( prev>=root->val){
+        result=false;
+        return ;
+    }
+    prev=root->val;
+     if(root->right){
+        Inorder(root->right,prev,result);
+     }
+}
+
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+        if(root->left==NULL && root->right==NULL){
+            return true;
+        }
+        long long prev=LLONG_MIN;
+        bool result=true;
+      Inorder(root,prev,result);
+      return result;
     }
 };
