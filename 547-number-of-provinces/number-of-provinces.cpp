ed011e48c29cdb2071f94b9e37c5306703b01638
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-14 22:58:08 +0530

    Time: 7 ms (11.93%) | Memory: 21.2 MB (9.45%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,6 +1,5 @@
 //Approach-1 BY USING MAP AND DFS-------------
-
-class Solution {
+/*class Solution {
 public:
  void dfs(unordered_map<int,vector<int>>adj,int u, vector<bool>&visited){
     visited[u]=true;
@@ -102,7 +101,8 @@ public:
 };
 
 Approach-4 BFS AND USING MAP -------------
-
+*/
+/*
 class Solution {
 public:
    void BFS( vector<vector<int>>&adj,int u,vector<bool>&visited){
@@ -117,7 +117,7 @@ public:
                  visited[v]=true;
                 q.push(v);
             }
-        }
+        
      }
    }
     int findCircleNum(vector<vector<int>>& isConnected) {
@@ -130,6 +130,40 @@ public:
                 count++;
             }
         }
+       return count;
+    }
+};
+*/
+
+class Solution {
+public:
+  void DFS(unordered_map<int,vector<int>>&adj,int u,vector<bool>&visited){
+    visited[u]=true;
+     for(int v:adj[u]){
+        if(!visited[v]){
+            DFS(adj,v,visited);
+        }
+     }
+   }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+       int n=isConnected.size();
+       unordered_map<int,vector<int>>adj;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+       }
+       vector<bool>visited(n,false);
+       int count=0;
+       for(int i=0;i<n;i++){
+        if(!visited[i]){
+            count++;
+            DFS(adj,i,visited);
+        }
+       }
        return count;
     }
 };
