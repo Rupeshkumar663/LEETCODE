cef3d806f1a95eb31048a0ae0eef83e61807baab
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-14 20:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (85.78%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -42,6 +42,7 @@ int m,n;
 //Time Complexity: O(m*n)
 //Space Complexity: O(m*n)
 //Recursion+Memoization-----------
+/*
 class Solution {
 public:
 int m,n;
@@ -80,5 +81,48 @@ int m,n;
         dp=vector<vector<pair<ll,ll>>>(m,vector<pair<ll,ll>>(n,{LLONG_MIN,LLONG_MAX}));
         auto [maxprod,minprod]=solve(grid,0,0);
         return maxprod<0 ? -1 : maxprod % MOD;
+    }
+};
+*/
+//Time Complexity: O(m*n)
+//Space Complexity: O(m*n)
+//Bottom-Up-----------
+class Solution {
+public:
+int m,n;
+  typedef long long ll;
+  long long MOD=1e9+7;
+    int maxProductPath(vector<vector<int>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        vector<vector<pair<ll,ll>>>dp(m,vector<pair<ll,ll>>(n));
+        dp[0][0]={grid[0][0],grid[0][0]};
+        //Column filled--------------
+        for(int j=1;j<n;j++){
+            dp[0][j]={dp[0][j-1].first*grid[0][j],dp[0][j-1].second*grid[0][j]};
+        }
+
+        //row filled--------------
+        for(int i=1;i<m;i++){
+            dp[i][0]={dp[i-1][0].first*grid[i][0],dp[i-1][0].second*grid[i][0]};
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                ll upmax=dp[i-1][j].first*grid[i][j];
+                ll upmin=dp[i-1][j].second*grid[i][j];
+                ll leftmax=dp[i][j-1].first*grid[i][j];
+                ll leftmin=dp[i][j-1].second*grid[i][j];
+                ll allmax=max({upmax,upmin,leftmax,leftmin});
+                ll allmin=min({upmax,upmin,leftmax,leftmin});
+                dp[i][j].first=allmax;
+                dp[i][j].second=allmin;
+            }
+        }
+        ll maxproduct=dp[m-1][n-1].first;
+        if(maxproduct<0){
+            return -1;
+        }
+        return maxproduct%MOD;
     }
 };
