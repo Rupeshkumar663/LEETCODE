6bd72af32b02f28b4bf004189cbcc761d75f8597
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-24 05:21:20 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (97.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,43 +2,50 @@
 
 class Solution {
 public:
-  void DFS(vector<vector<int>>& grid ,int i,int j,int m,int n,int count){
-    if(i<0 || j<0 || i>=m|| j>=n)
-        return;
-
-    if(grid[i][j]==0)
-       return ;
-    
-    if(grid[i][j]!=1 && grid[i][j]<2+count){
-        return ;
-     }
-     grid[i][j]=2+count;
-
-      DFS(grid,i,j-1,m,n,count+1);//left
-      DFS(grid,i,j+1,m,n,count+1);//right
-      DFS(grid,i-1,j,m,n,count+1);//up
-      DFS(grid,i+1,j,m,n,count+1);//down
-   }
+  
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-       for(int i=0;i<m;i++){
+    int m=grid.size();
+    int n=grid[0].size();
+    queue<pair<int,int>>q;
+    for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(grid[i][j]==2)
-               DFS(grid,i,j,m,n,0);
+                q.push({i,j});
+        }
+    }
+    int result=0;
+    while(!q.empty()){
+        int t=q.size();
+        while(t--){
+         pair<int,int>p=q.front();
+         q.pop();
+         int i=p.first;
+         int j=p.second;
+         if(i-1>=0 && grid[i-1][j]==1){//upward
+             grid[i-1][j]=2;
+            q.push({i-1,j});
          }
-       } 
-      
-       int result=0;
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(grid[i][j]==1)
-               return -1;
-
-            if(grid[i][j]>=2)
-               result=max(result,grid[i][j]-2);
+         if(i+1<m && grid[i+1][j]==1){//downward
+             grid[i+1][j]=2;
+            q.push({i+1,j});
+         }
+         if(j-1>=0 && grid[i][j-1]==1){//left
+            grid[i][j-1]=2;
+            q.push({i,j-1});
+         }
+         if(j+1<n && grid[i][j+1]==1){//right
+            grid[i][j+1]=2;
+            q.push({i,j+1});
           }
-       } 
-       return result;
+        }
+       result++;
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==1 )
+                return -1;
+        }
+    }
+    return result==0?result:result-1;
   }
 };
