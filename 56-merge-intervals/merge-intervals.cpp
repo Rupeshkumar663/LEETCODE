b5e98caee5742bf1c007584883106c4d459ec09a
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-23 11:00:28 +0530

    Time: 4 ms (65.93%) | Memory: 23.9 MB (59.38%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,21 +1,23 @@
+//Time complexity: O(nlogn)+O(n)=O(nlogn)
+//Space complexity: O(n)
+//Greedy Approach--------------
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-
-        vector<vector<int>>result;
+        sort(intervals.begin(),intervals.end());//Time complexity: O(nlogn)
         int start=intervals[0][0];
         int end=intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]<=end){
-               end=max(end,intervals[i][1]);
-            } else{
-                  result.push_back({start,end});
-                  start=intervals[i][0];
-                  end=intervals[i][1];
+        vector<vector<int>>result;//Space complexity: O(n)
+        for(int i=1;i<intervals.size();i++){//Time complexity: O(n)
+            if(end>=intervals[i][0]){
+                end=max(end,intervals[i][1]);
+            }else{
+                result.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
             }
         }
-       result.push_back({start,end});
-       return result;
+         result.push_back({start,end});
+         return result;
     }
 };
