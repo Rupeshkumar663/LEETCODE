17b0648975cb3af65c5282f7dc04c1a6deeaf7cb
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-26 11:54:00 +0530

    Time: 210 ms (95.59%) | Memory: 90.3 MB (77.73%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.cpp
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.cpp
@@ -1,16 +1,18 @@
+//Time Complexity: O(n+(k*logn)+(n*logn))=O(nlogn)
+//Space Complexity:O(n)
 class Solution {
 public:
 long long MOD=1e9+7;
     int maximumProduct(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
-        while(k--){
+        priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());//Time Complexity:O(n*logn)
+        while(k--){//Time Complexity:O(k*logn)
             int temp=pq.top();
-            pq.pop();
+            pq.pop();//Time Complexity:O(logn)
             temp++;
-            pq.push(temp);
+            pq.push(temp);//Time Complexity:O(logn)
         }
         long long multi=1;
-        while(!pq.empty()){
+        while(!pq.empty()){//Time Complexity:O(n)
             long long x=(long long)pq.top();
             pq.pop();
             multi=(multi*x)%MOD;
