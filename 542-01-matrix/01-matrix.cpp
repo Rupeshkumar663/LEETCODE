b6758b39863a77ae028de4577c20193d0c7d738a
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-27 21:09:02 +0530

    Time: 180 ms (5.02%) | Memory: 75 MB (5.05%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,24 +1,35 @@
 class Solution {
-public:
+public:   
+   
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size(),n=mat[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> dist(m,vector<int>(n,-1));
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                if(mat[i][j]==0)
-                    q.push({i,j}),dist[i][j]=0;
-        int dx[4]={1,-1,0,0};
-        int dy[4]={0,0,1,-1};
-        while(!q.empty()){
-            auto p=q.front();q.pop();
-            for(int k=0;k<4;k++){
-                int nx=p.first+dx[k],ny=p.second+dy[k];
-                if(nx>=0&&ny>=0&&nx<m&&ny<n&&dist[nx][ny]==-1)
-                    dist[nx][ny]=dist[p.first][p.second]+1,
-                    q.push({nx,ny});
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>result(m,vector<int>(n,-1));
+        queue<pair<int,int>>q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    result[i][j]=0;
+                    q.push({i,j});
+                }
             }
         }
-        return dist;
+         vector<vector<int>>dir={{-1,0},{1,0},{0,-1},{0,1}};
+         while(!q.empty()){
+            pair<int,int>temp=q.front();
+            q.pop();
+            int i=temp.first;
+            int j=temp.second;
+            for(auto direction:dir){
+                int x=i+direction[0];
+                int y=j+direction[1];
+                if(x>=0 && x<m && y>=0&& y<n && result[x][y]==-1){
+                    result[x][y]=result[i][j]+1;
+                    q.push({x,y});
+                }
+            }
+         }
+         return result;
     }
 };
