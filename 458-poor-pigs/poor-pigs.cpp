4000f6f830b9402649a4496050384efc137be367
Rupesh Kumar <samratrupesh08@gmail.com>
2026-07-29 08:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.08%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int rounds=minutesToTest/minutesToDie;
+        int pigs=0;
+        int states=1;
+        while (states<buckets){
+            pigs++;
+            states *=(rounds+1);
+        }
+        return pigs;
+    }
+};
