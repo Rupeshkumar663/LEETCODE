1358c251e5f73d64d392e88267d482b1f063fcde
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-01 09:43:58 +0530

    Time: 59 ms (8.27%) | Memory: 19.3 MB (7.58%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,4 +1,4 @@
-//Time Complexity:O(n)*O(k)=O(n)
+/*//Time Complexity:O(n)*O(k)=O(n)
 //Space Complexity: O(n)
 //Sliding Window Approach-------------------
 class Solution {
@@ -16,5 +16,31 @@ public:
           result=max(result,j-i+1);
        }
        return result;
+    }
+};*/
+
+
+//Time Complexity:O(n)*O(k)=O(n)
+//Space Complexity: O(n)
+//Sliding Window Approach-------------------
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+      unordered_map<char,int>m;
+      int i=0,j=0;
+      int count=0;
+      while(j<n){
+          if(m.find(s[j])!=m.end()){
+            if(m[s[j]]>=i){
+               i=m[s[j]]+1;
+               m[s[j]]=j;
+             }
+          }
+          m[s[j]]=j;
+          count=max(count,j-i+1);
+          j++;
+      }
+      return count;
     }
 };
