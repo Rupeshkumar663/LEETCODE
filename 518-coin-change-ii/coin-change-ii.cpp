d25e00b1e38b4cbbe03ee6c1f3c3d3d1dfca84be
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-04 20:48:55 +0530

    Time: 16 ms (77.74%) | Memory: 16.3 MB (74.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -20,6 +20,7 @@ public:
     }
 };*/
 
+/*
 //Time COmplexity: O(n*n)
 //Space Complexity: O(n*n)
 //By Recursion +Memoization Approach------------
@@ -45,5 +46,34 @@ int dp[301][5001];
     int change(int amount, vector<int>& coins) {
         memset(dp,-1,sizeof(dp));
      return solve(0,amount,coins); 
+    }
+};*/
+
+//Time COmplexity: O(n*n)
+//Space Complexity: O(n*n)
+//By Recursion +Memoization Approach------------
+class Solution {
+public:
+int dp[301][5001];
+ int solve(int i,int amount, vector<int>& coins){
+    if(amount==0)
+       return 1;
+    if(i<0)
+      return 0;
+
+    if(dp[i][amount]!=-1){
+        return dp[i][amount];
+    }
+    int take=0;
+    if(amount>=coins[i]){
+        take=solve(i,amount-coins[i],coins);
+    }
+    int skip=solve(i-1,amount,coins);
+    return dp[i][amount]=skip+take;
+ } 
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        memset(dp,-1,sizeof(dp));
+     return solve(n-1,amount,coins); 
     }
 };
