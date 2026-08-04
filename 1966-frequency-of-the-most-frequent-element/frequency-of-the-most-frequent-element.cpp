7e572a740aac21fd166cbc9ba6a3afea573f95b9
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-04 11:44:30 +0530

    Time: 41 ms (48.12%) | Memory: 105.8 MB (63.94%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,41 @@
+//Time Complexity: O(n*n)
+//Space COmplexity: O(1)
+//Brute Force Approach----------
+/*class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int maxi=0;
+        for(int i=0;i<nums.size();i++){
+            long long sum=0;
+            for(int j=i;j<nums.size();j++){
+                sum+=nums[j];
+                if((1LL*nums[j]*(j-i+1)-sum)<=k)
+                    maxi=max(maxi,j-i+1);
+                else
+                    break;
+            }
+        }
+        return maxi;
+    }
+};*/
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int maxi=0;
+        long long sum=0;
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+                sum+=nums[j];
+                if((1LL*nums[j]*(j-i+1)-sum)<=k)
+                    maxi=max(maxi,j-i+1);
+                while((1LL*nums[j]*(j-i+1)-sum)>k){
+                    sum-=nums[i];
+                    i++;
+                }
+           }
+        return maxi;
+    }
+};
