12958df8d8fcb0da1b9ec584429711e28852f30d
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-08 13:29:55 +0530

    Time: 9 ms (20.37%) | Memory: 15.5 MB (49.42%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,56 +9,61 @@
  */
 class Solution {
 public:
-    void Inorder(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&m){
-        if(!root)
-          return;
-       
-        if(root->left){
-            m[root->left]=root;
-           Inorder(root->left,m);
-        }
-        if(root->right){
-            m[root->right]=root;
-           Inorder(root->right,m);
-        }
+ void Inorder(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&m){
+    if(root==NULL){
+        return;
     }
-    void finddistance(int k,vector<int>&result,TreeNode* target,  unordered_map<TreeNode*,TreeNode*>&m){
-        queue<TreeNode*>q;
-        q.push(target);
-        unordered_set<int>visited;
-        visited.insert(target->val);
-        while(!q.empty()){
-            int n=q.size();
-            if(k==0)
-             break;
-              while(n--){
-               TreeNode* temp=q.front();
-               q.pop();
-               if(temp->left && !visited.count(temp->left->val)){
-                q.push(temp->left);
-                visited.insert(temp->left->val);
-                }
-              if(temp->right && !visited.count(temp->right->val)){
-               q.push(temp->right);
-                 visited.insert(temp->right->val);
-                }
-               if(m.count(temp) && !visited.count(m[temp]->val)){
-                 q.push(m[temp]);
-                 visited.insert(m[temp]->val);
-                }
-             }
-             k--;
-        }
-        while(!q.empty()){
-            result.push_back(q.front()->val);
+    if(root->left){
+        m[root->left]=root;
+        Inorder(root->left,m);
+    }
+     if(root->right){
+        m[root->right]=root;
+        Inorder(root->right,m);
+    }
+  }
+
+  void BFS(TreeNode* root, TreeNode* target, int k,unordered_map<TreeNode*,TreeNode*>&m,unordered_map<TreeNode*,bool>&visited,vector<int>&result){
+    queue<TreeNode*>q;
+    q.push(target);
+    visited[target]=true;
+    while(!q.empty()){
+        int t=q.size();
+        while(t--){
+            TreeNode*temp=q.front();
             q.pop();
-        }
-    }
+            if(temp->left && visited[temp->left]==false){
+                visited[temp->left]=true;
+                q.push(temp->left);
+            }
+            if(temp->right && visited[temp->right]==false){
+                 visited[temp->right]=true;
+                q.push(temp->right);
+            }
+            if(m[temp] && visited[m[temp]]==false){
+                 visited[m[temp]]=true;
+                q.push(m[temp]);
+            }
+         }
+         k--;
+         if(k==0)
+           break;
+     }
+      while(!q.empty()){
+        TreeNode* p=q.front();
+        q.pop();
+        result.push_back(p->val);
+      }
+  }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-      unordered_map<TreeNode*,TreeNode*>m;
-      vector<int>result;
-      Inorder(root,m); 
-      finddistance(k,result,target,m); 
-      return result;
+        if(k==0){
+            return {target->val};
+        }
+        unordered_map<TreeNode*,TreeNode*>m;
+        Inorder(root,m);
+        unordered_map<TreeNode*,bool>visited;
+        vector<int>result;
+        BFS(root,target,k,m,visited,result);
+        return result;
     }
 };
