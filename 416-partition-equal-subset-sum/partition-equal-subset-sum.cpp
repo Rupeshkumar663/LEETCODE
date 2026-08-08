1d1b11bb59f2dc4d0453ddf60a23dea4c597336f
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-08 15:34:14 +0530

    Time: 143 ms (42.04%) | Memory: 17.4 MB (66.30%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -70,7 +70,7 @@ int dp[201][10001];
        return Solve(nums,0,sum/2);
     }
 };*/
-//Time Complexity: O(2^n)
+/*//Time Complexity: O(2^n)
 //Space Complexity: O(1)
 //Recursion +Memoization Approach--------------------
 //Move Right->Left
@@ -106,5 +106,38 @@ int dp[201][100001];
        }
        memset(dp,-1,sizeof(dp));
        return Solve(nums,n-1,sum/2);
+    }
+};
+*/
+
+//Time Complexity: O(2^n)
+//Space Complexity: O(1)
+//Recursion +Memoization Approach--------------------
+//Move Right->Left
+class Solution {
+public:
+
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+       int sum=accumulate(nums.begin(),nums.end(),0);
+       if(sum%2!=0){
+        return false;
+       }
+       vector<vector<bool>>dp(n+1,vector<bool>((sum/2)+1,false));
+       for(int i=0;i<n;i++){
+        dp[i][0]=true;
+       }
+       for(int i=1;i<n+1;i++){
+        for(int j=1;j<dp[0].size()+1;j++){
+            bool take=false;
+           if(j>=nums[i-1]){
+              take=dp[i-1][j-nums[i-1]];
+            }
+          bool skip=dp[i-1][j];
+    
+         dp[i][j]=take || skip;
+        }
+       }
+       return dp[n][sum/2];
     }
 };
