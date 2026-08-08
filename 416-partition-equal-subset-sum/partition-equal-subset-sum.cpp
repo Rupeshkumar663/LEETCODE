40e77103844feb762f777fa8d866cea9f415c4a8
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-08 15:13:07 +0530

    Time: 990 ms (5.09%) | Memory: 101.4 MB (47.50%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,110 @@
+/*
+
+//Time Complexity: O(2^n)
+//Space Complexity: O(1)
+//Recursion Approach--------------------
 class Solution {
 public:
-   bool isSubsetSum(vector<int>& nums, int sum) {
-       int n=nums.size();
-       vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
-       dp[0][0]=true;
-       for(int i=1;i<n+1;i++){
-           dp[i][0]=true;
-       }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                bool take=false;
-                if(j>=nums[i-1]){
-                  take=dp[i-1][j-nums[i-1]];
-                 }
-                 bool skip=dp[i-1][j];
-                 dp[i][j]=skip || take;
-            }
-        }
-        return dp[n][sum];
+   bool Solve(vector<int>&nums,int i,int sum){
+    //Base Case of Recursion Tree
+    if(sum==0){
+        return true;
     }
+    if(i==nums.size()){
+        return false;
+    }
+   //Recursive Case
+    bool take;
+    if(sum>=nums[i]){
+        take=Solve(nums,i+1,sum-nums[i]);
+    }
+    bool skip=Solve(nums,i+1,sum);
+    //Return Case
+    return take || skip;
+   }
     bool canPartition(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0){
-            return false;
-        }
-       return isSubsetSum(nums,sum/2);
+       int sum=accumulate(nums.begin(),nums.end(),0);
+       if(sum%2!=0){
+        return false;
+       }
+       return Solve(nums,0,sum/2);
+    }
+};*/
+
+/*
+
+//Time Complexity: O(2^n)
+//Space Complexity: O(1)
+//Recursion +Memoization Approach--------------------
+//Move Left->Right
+class Solution {
+public:
+int dp[201][10001];
+   bool Solve(vector<int>&nums,int i,int sum){
+    //Base Case of Recursion Tree
+    if(sum==0){
+        return true;
+    }
+    if(i==nums.size()){
+        return false;
+    }
+
+    if(dp[i][sum]!=-1){
+        return dp[i][sum];
+    }
+   //Recursive Case
+    bool take;
+    if(sum>=nums[i]){
+        take=Solve(nums,i+1,sum-nums[i]);
+    }
+    bool skip=Solve(nums,i+1,sum);
+    //Return Case
+    return dp[i][sum]=take || skip;
+   }
+    bool canPartition(vector<int>& nums) {
+       int sum=accumulate(nums.begin(),nums.end(),0);
+       if(sum%2!=0){
+        return false;
+       }
+       memset(dp,-1,sizeof(dp));
+       return Solve(nums,0,sum/2);
+    }
+};*/
+//Time Complexity: O(2^n)
+//Space Complexity: O(1)
+//Recursion +Memoization Approach--------------------
+//Move Right->Left
+class Solution {
+public:
+int dp[201][100001];
+   bool Solve(vector<int>&nums,int i,int sum){
+    //Base Case of Recursion Tree
+    if(sum==0){
+        return true;
+    }
+    if(i==0){
+        return false;
+    }
+
+    if(dp[i][sum]!=-1){
+        return dp[i][sum];
+    }
+   //Recursive Case
+    bool take;
+    if(sum>=nums[i-1]){
+        take=Solve(nums,i-1,sum-nums[i-1]);
+    }
+    bool skip=Solve(nums,i-1,sum);
+    //Return Case
+    return dp[i][sum]=take || skip;
+   }
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+       int sum=accumulate(nums.begin(),nums.end(),0);
+       if(sum%2!=0){
+        return false;
+       }
+       memset(dp,-1,sizeof(dp));
+       return Solve(nums,n-1,sum/2);
     }
 };
