c5470c2780f28f3026da6ec95991edb611e81f24
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-09 21:05:23 +0530

    Time: 167 ms (29.52%) | Memory: 21.5 MB (29.90%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>wordbank(wordList.begin(),wordList.end());
-        
+        unordered_set<string>wordBank(wordList.begin(),wordList.end());
         unordered_set<string>visited;
         queue<string>q;
-        int count=1;
         q.push(beginWord);
         visited.insert(beginWord);
+        int count=1;
         while(!q.empty()){
-            int n=q.size();
-          while(n--){
-             string temp=q.front();
-             q.pop();
-            if(temp==endWord){
+          int t=q.size();
+          while(t--){
+            string temp1=q.front();
+            q.pop();
+            if(temp1==endWord){
                 return count;
-             } 
-             for(char ch:"abcdefghijklmnopqrstuvwxyz"){
-                    for(int i=0;i<temp.length();i++){
-                        string ss=temp;
-                        ss[i]=ch;
-                        if(visited.find(ss)==visited.end() && wordbank.find(ss)!=wordbank.end()){
-                            q.push(ss);
-                            visited.insert(ss);
-                        }
+            }
+            for(auto ch:"abcdefghijklmnopqrstuvwxyz"){
+                for(int i=0;i<temp1.size();i++){
+                    string temp2=temp1;
+                    temp2[i]=ch;
+                    if(visited.find(temp2)==visited.end() && wordBank.find(temp2)!=wordBank.end()){
+                        visited.insert(temp2);
+                        q.push(temp2);
                     }
-                } 
+                }
+            }
           }
-       count++;
+          count++;
         }
-        return 0;
+       return 0;
     }
 };
