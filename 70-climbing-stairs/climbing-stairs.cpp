9f540d903b80667af71a5c154700ffba8c00d45b
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-14 13:31:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -111,9 +111,9 @@ public:
       return solve(n);
     }
 };
-*/
-//Time COmplexity: O(2^n)
-//Space COmplexity: O(1)
+
+//Time COmplexity: O(n)
+//Space COmplexity: O(n)
 //Recursion +Memoization Approach---------
 class Solution {
 public: 
@@ -138,5 +138,26 @@ int dp[46];
     int climbStairs(int n) {
         memset(dp,-1,sizeof(dp));
       return solve(n);
+    }
+};
+*/
+//Time COmplexity: O(n)
+//Space COmplexity: O(n)
+//Recursion +Memoization Approach---------
+class Solution {
+public: 
+
+    int climbStairs(int n) {
+      vector<int>dp(n+1,-1);
+      if(n==1 || n==2){
+        return n;
+      }
+      dp[0]=0;
+      dp[1]=1;
+      dp[2]=2;
+      for(int i=3;i<n+1;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+      }
+     return dp[n];
     }
 };
