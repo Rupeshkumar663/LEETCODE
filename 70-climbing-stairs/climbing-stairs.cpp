23b9fd608f2f48fa936ce5978e14dd9293ffb117
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-14 13:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.03%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -77,23 +77,66 @@ public:
      return c;
     }
 };
-*/
+
 class Solution {
 public: 
-int solve(int n,vector<int>&dp){
-    if(n<0){
-        return 0;
-    }
-    if(n==0){
-        return 1;
-    }
-    if(dp[n]!=-1)
-      return dp[n];
-    return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-  }
+
     int climbStairs(int n) {
       vector<int>dp(n+1,-1);
      return solve(n,dp);
       
+    }
+};
+
+//Time COmplexity: O(2^n)
+//Space COmplexity: O(1)
+//Recursion Approach---------
+class Solution {
+public: 
+  int solve(int n){
+    if(n==0){
+        return 1;
+    }
+     int one=0;
+     int two=0;
+     if(n>=1){
+       one=solve(n-1);
+     }
+      if(n>=2){
+       two=solve(n-2);
+     }
+    return one+two;
+  }
+    int climbStairs(int n) {
+      return solve(n);
+    }
+};
+*/
+//Time COmplexity: O(2^n)
+//Space COmplexity: O(1)
+//Recursion +Memoization Approach---------
+class Solution {
+public: 
+int dp[46];
+  int solve(int n){
+    if(n==0){
+        return 1;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+     int one=0;
+     int two=0;
+     if(n>=1){
+       one=solve(n-1);
+     }
+      if(n>=2){
+       two=solve(n-2);
+     }
+    return dp[n]=one+two;
+  }
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+      return solve(n);
     }
 };
