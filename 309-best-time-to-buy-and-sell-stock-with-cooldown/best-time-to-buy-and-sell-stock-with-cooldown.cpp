71f955970fff4fc5ca415dee4028e9c901214d7e
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-18 09:27:17 +0530

    Time: 2 ms (39.22%) | Memory: 15.6 MB (72.62%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,62 @@
+/*//Time COmplexity: O(2^n)
+//SPace COmplexity: O(1)
+//Recursion Approach------------
+class Solution {
+public:
+ int n;
+ int solve(int i,bool buy,vector<int>&prices){
+    if(i>=n){
+        return 0;
+    }
+    int profit=0;
+    int take=0;
+    if(buy==true){//buy-----------------
+        take=solve(i+1,false,prices)-prices[i];
+        int not_take=solve(i+1,true,prices);
+        profit=max({profit,take,not_take});
+    }else{
+        take=solve(i+2,true,prices)+prices[i];
+        int not_take=solve(i+1,false,prices);
+        profit=max({profit,take,not_take});
+    }
+    return profit;
+ }
+    int maxProfit(vector<int>& prices) {
+     n=prices.size();
+     return solve(0,true,prices);
+    }
+};*/
+
+//Time COmplexity: O(2^n)
+//SPace COmplexity: O(1)
+//Recursion Approach------------
+class Solution {
+public:
+ int n;
+ int dp[5001][2];
+ int solve(int i,bool buy,vector<int>&prices){
+    if(i>=n){
+        return 0;
+    }
+    if(dp[i][buy]!=-1){
+        return dp[i][buy];
+    }
+    int profit=0;
+    int take=0;
+    if(buy==true){//buy-----------------
+        take=solve(i+1,false,prices)-prices[i];
+        int not_take=solve(i+1,true,prices);
+        profit=max({profit,take,not_take});
+    }else{
+        take=solve(i+2,true,prices)+prices[i];
+        int not_take=solve(i+1,false,prices);
+        profit=max({profit,take,not_take});
+    }
+    return dp[i][buy]=profit;
+ }
+    int maxProfit(vector<int>& prices) {
+     n=prices.size();
+     memset(dp,-1,sizeof(dp));
+     return solve(0,true,prices);
+    }
+};
