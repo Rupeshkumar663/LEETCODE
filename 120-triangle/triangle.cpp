a4f2d47f91bc19ecef37b5c6dccf6ef487b6dc8d
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-18 12:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (26.34%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,4 +1,63 @@
+/*
+//Time Complexity: O(2^n)
+//Space Complexity: O(1)
+//Bottom-Up Approach----------
+class Solution {
+public:
+  int n;
+  int Solve(int i,int j,vector<vector<int>>& triangle){
+    if(i==n-1){
+        return triangle[i][j];
+    }
+    int current=Solve(i+1,j,triangle);
+    int next=Solve(i+1,j+1,triangle);
+    return triangle[i][j]+min(current,next);
+  }
+    int minimumTotal(vector<vector<int>>& triangle) {
+         n=triangle.size();
+       return  Solve(0,0,triangle);
+    }
+};
 
+*/
+
+
+//Time Complexity: O(n*n)
+//Space Complexity: O(n*n)
+//Top-Down Approach (Recursion + Memoization)
+
+class Solution {
+public:
+  int n;
+  vector<vector<int>>dp;
+  int Solve(int i,int j,vector<vector<int>>& triangle){
+    if(i==n-1){
+        return triangle[i][j];
+    }
+    if(dp[i][j]!=INT_MAX){
+        return dp[i][j];
+    }
+    int current=Solve(i+1,j,triangle);
+    int next=Solve(i+1,j+1,triangle);
+    return dp[i][j]=triangle[i][j]+min(current,next);
+  }
+    int minimumTotal(vector<vector<int>>& triangle) {
+         n=triangle.size();
+         dp.assign(n,vector<int>(n,INT_MAX));
+       return  Solve(0,0,triangle);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+/*
 //Time Complexity: O(n*n)
 //Space Complexity: O(n*n)
 //Bottom-Up Approach----------
@@ -14,4 +73,4 @@ public:
         }
         return dp[0];
     }
-};
+};*/
