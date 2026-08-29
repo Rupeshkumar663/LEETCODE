7bc07a109e9c150d3b68ea6edcf505e0d8174c47
Rupesh Kumar <samratrupesh08@gmail.com>
2026-08-29 09:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (81.02%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,16 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+           char temp=s[i];
+           s[i]=s[j];
+           s[j]=temp;
+           i++;
+           j--;
+        }
+    }
+};
