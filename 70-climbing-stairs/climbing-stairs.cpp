e7ab8daf6916d799133ec659f91820f46c0baaed
Rupesh Kumar <samratrupesh08@gmail.com>
2026-09-03 14:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.48%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -146,18 +146,23 @@ int dp[46];
 //Bottom-Up Approach---------
 class Solution {
 public: 
-
+int dp[46];
+   int Solve(int i,int &n){
+    if(i==n){
+        return 1;
+    }
+    if(i>n){
+      return 0;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int one=Solve(i+1,n);
+    int two=Solve(i+2,n);
+      return dp[i]=one+two;
+   }
     int climbStairs(int n) {
-      vector<int>dp(n+1,-1);
-      if(n==1 || n==2){
-        return n;
-      }
-      dp[0]=0;
-      dp[1]=1;
-      dp[2]=2;
-      for(int i=3;i<n+1;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-      }
-     return dp[n];
+        memset(dp,-1,sizeof(dp));
+     return Solve(0,n);
     }
 };
