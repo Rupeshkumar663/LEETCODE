1793cace716aa538c11d0600ebda739b53955e71
Rupesh Kumar <samratrupesh08@gmail.com>
2026-09-03 09:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (94.20%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -114,7 +114,7 @@ public:
 
 /*//Time COmplexity: O(2^(m+n))
 //Space Complexity: O(1)
-//Buttom-Up Approach-----------------
+//Recursion Approach-----------------
 class Solution {
 public:
   int m,n;
@@ -139,9 +139,9 @@ public:
     }
 };*/
 
-//Time COmplexity: O(2^(m+n))
+/*//Time COmplexity: O(m*n)
 //Space Complexity: O(1)
-//Buttom-Up Approach-----------------
+//Recursion+ Memoization Approach-----------------
 class Solution {
 public:
   int m,n;
@@ -168,5 +168,90 @@ public:
       }
       memset(dp,-1,sizeof(dp));
      return  Solve(0,0,obstacleGrid);
+    }
+};*/
+
+/*//Time COmplexity: O(m*n)
+//Space Complexity: O(m*n)
+//Buttom-Up Approach-----------------
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+      int  m=obstacleGrid.size();
+      int n=obstacleGrid[0].size();
+      if(obstacleGrid[m-1][n-1]==1){
+        return 0;
+       }
+      vector<vector<int>>dp(m,vector<int>(n));
+      dp[0][0]=1;
+      // 1st row filled-----------
+      for(int j=1;j<n;j++){
+        if(obstacleGrid[0][j]==1){
+            dp[0][j]=0;
+        }
+        else if(j>=1 && obstacleGrid[0][j-1]==1){
+            dp[0][j]=0;
+            obstacleGrid[0][j]=1;
+        }else{
+            dp[0][j]=1;
+        }
+      }
+       //1st column filled-----------
+       for(int i=1;i<m;i++){
+        if(obstacleGrid[i][0]==1){
+            dp[i][0]=0;
+        }
+        else if(i>=1 && obstacleGrid[i-1][0]==1){
+            dp[i][0]=0;
+            obstacleGrid[i][0]=1;
+        }else{
+            dp[i][0]=1;
+        }
+      }
+
+      //rest part will be filled----------
+       for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(obstacleGrid[i][j]==1){
+                dp[i][j]=0;
+            }else{
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+       }
+       return dp[m-1][n-1];
+    }
+};
+*/
+
+//Time COmplexity: O(m*n)
+//Space Complexity: O(m*n)
+//Buttom-Up Approach-----------------
+class Solution {
+public:
+ int m,n;
+ int dp[101][101];
+   int Solve(int i,int j,vector<vector<int>>& obstacleGrid){
+       if(i==m-1 && j==n-1){
+        return 1;
+      }
+      if(i>=m || j>=n || obstacleGrid[i][j]==1){
+        return 0;
+      }
+      if(dp[i][j]!=-1){
+        return dp[i][j];
+      }
+      int right=Solve(i+1,j,obstacleGrid);
+      int left=Solve(i,j+1,obstacleGrid);
+      return dp[i][j]=left+right;
+   }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+       m=obstacleGrid.size();
+       n=obstacleGrid[0].size();
+       if(obstacleGrid[m-1][n-1]==1){
+        return 0;
+       }
+       memset(dp,-1,sizeof(dp));
+      return Solve(0,0,obstacleGrid);
     }
 };
