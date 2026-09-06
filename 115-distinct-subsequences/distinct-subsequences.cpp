52a0c41bc8f1c3f9898d756c55e449940fa5f573
Rupesh Kumar <samratrupesh08@gmail.com>
2026-09-06 09:00:04 +0530

    Time: 28 ms (63.03%) | Memory: 13 MB (80.54%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int n,m;
+int dp[1001][1001];
+   int Solve(int i,int j,string &s, string &t){
+    if(j==m){
+        return 1;
+    }
+    if(i==n){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    int take=0;
+    if(s[i]==t[j]){
+      take=Solve(i+1,j+1,s,t);
+    }
+    int skip=Solve(i+1,j,s,t);
+    return dp[i][j]=take+skip;
+   }
+    int numDistinct(string s, string t) {
+        n=s.size();
+        m=t.size();
+        memset(dp,-1,sizeof(dp));
+       return Solve(0,0,s,t); 
+    }
+};
