81dff4f4012356794a1484391c87c36727d51eee
Rupesh Kumar <samratrupesh08@gmail.com>
2026-09-13 17:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.46%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,4 +1,7 @@
-class Solution {
+
+//Time Complexity :O(n)
+//Space Complexity :O(1)
+/*class Solution {
 public:
   double modpower(double x,long long n){
     double result=1.0;
@@ -18,5 +21,21 @@ public:
             return 1/result;
         }
         return modpower(x,n);
+    }
+};*/
+
+class Solution {
+public:
+ 
+    double myPow(double x, long long n) {
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+         return 1/myPow(x,-n);
+        }
+       if(n%2==0)
+        return myPow(x*x,n/2);
+        return x*myPow(x*x,(n-1)/2);
     }
 };
