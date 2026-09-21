fbdd636df0977ad691ca864b2fcbae356d669ea9
Rupesh Kumar <samratrupesh08@gmail.com>
2026-09-21 10:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.10%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int red=0;
@@ -19,5 +19,29 @@ public:
            nums.push_back(1);
            for(int i=0;i<blue;i++)
            nums.push_back(2);
+    }
+};*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red=0;
+        int white=0;
+        int blue=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+                red++;
+            else if(nums[i]==1)
+                white++;
+            else
+                blue++;
+        }
+        int index=0;
+        while(red--)
+            nums[index++]=0;
+        while(white--)
+            nums[index++]=1;
+        while(blue--)
+            nums[index++]=2;
     }
 };
